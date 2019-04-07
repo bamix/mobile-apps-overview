86036b4a230f8559c3e2ac34f6801964c8cbb760
Alex <alexignatyy@gmail.com>
2019-04-07 13:50:24 +0300

    Add Vue-Ionic and VueNative

--- react-native-app/screens/SettingsScreen.js
+++ react-native-app/screens/SettingsScreen.js
@@ -3,14 +3,14 @@ import {WebView} from 'react-native';
 
 export default class SettingsScreen extends React.Component {
   static navigationOptions = {
-    title: 'app.json',
+    title: 'Test',
   };
 
   render() {
     return (
       <WebView
         originWhitelist={['*']}
-        source={{ uri: "https://facebook.github.io/react-native/" }}
+        source={{ uri: "https://chill-dedication.glitch.me/" }}
       />
     );
   }
